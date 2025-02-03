6f8ce9131e6f7e6ee909c31f3fa94835f94ff569
Dorijan Di Zepp <dorijan.dizepp@gmail.com>
2025-02-03 10:30:25 +0100

    update README, added output folder

--- src/parallel.c
+++ src/parallel.c
@@ -1,12 +1,14 @@
 #include <mpi.h>
+#include <omp.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "common.h"
 #include "log.h"
-#include "sequential.h"
 #include "stencil.h"
 #include "types.h"
+
+extern uint32_t num_threads;
 
 Matrix compute_parallel(const Matrix matrix)
 {
@@ -18,37 +20,34 @@ Matrix compute_parallel(const Matrix matrix)
     MPI_Comm_size(MPI_COMM_WORLD, &procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Allocate starts, ends, counts and displacement arrays
+    // Allocate starts, ends and counts arrays
     uint32_t size = procs * sizeof(int32_t);
     int32_t *starts = malloc(size);
     int32_t *ends = malloc(size);
     int32_t *counts = malloc(size);
-    int32_t *displs = malloc(size);
 
     if (rank == 0) {
         // Allocate temporary matrix data buffer
-        tmp.data = malloc(matrix.total_size * sizeof(float));
+        uint32_t total_bytes = matrix.total_size * sizeof(float);
+        tmp.data = malloc(total_bytes);
+        memcpy(tmp.data, matrix.data, total_bytes);
 
         // Divide the iteration across available processes
         uint32_t iters = matrix.total_size / procs;
         uint32_t remainder = matrix.total_size % procs;
 
-        // Calculate starts, ends, counts and displacement arrays for every process
-        uint32_t total_count = 0;
+        // Calculate starts, ends and counts arrays for every process
         for (uint32_t i = 0; i < (uint32_t)procs; i++) {
             starts[i] = i * iters + (i < remainder ? i : remainder);
             ends[i] = starts[i] + iters + (i < remainder ? 1 : 0);
             counts[i] = ends[i] - starts[i];
-            displs[i] = total_count;
-            total_count += counts[i];
         }
     }
 
-    // Synchronize starts, ends, counts and displacement arrays
+    // Synchronize starts, ends and counts arrays
     MPI_Bcast(starts, procs, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(ends, procs, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(counts, procs, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(displs, procs, MPI_INT, 0, MPI_COMM_WORLD);
 
     uint32_t start = starts[rank];
     uint32_t end = ends[rank];
@@ -63,15 +62,15 @@ Matrix compute_parallel(const Matrix matrix)
         double start_time = MPI_Wtime();
 
         // For every element calculate the new value
-        #pragma omp parallel for num_threads(2) schedule(guided)
-        for (uint32_t i = start; i < end; i++)
+        //#pragma omp parallel for num_threads(2) schedule(guided)
+        for (uint32_t j = start; j < end; j++)
         {
-            local_data[i - start] = compute_stencil(matrix, i);
+            local_data[j - start] = compute_stencil(tmp, j);
         }
 
         // Gather computed array data to the root process
         // Synchronize processes at the end of every iteration
-        MPI_Gatherv(local_data, count, MPI_FLOAT, tmp.data, counts, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Allgatherv(local_data, count, MPI_FLOAT, tmp.data, counts, starts, MPI_FLOAT, MPI_COMM_WORLD);
 
         if (rank == 0) {
             double time = MPI_Wtime() - start_time;
@@ -85,7 +84,6 @@ Matrix compute_parallel(const Matrix matrix)
     free(starts);
     free(ends);
     free(counts);
-    free(displs);
     free(local_data);
     MPI_Finalize();
 
